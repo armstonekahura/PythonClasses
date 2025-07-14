958eade4ed49e3995201f90b7413468598259d48
armstonekahura <armstonekahura@gmail.com>
2025-07-14 13:49:50 +0300

    Scope added

--- pythonOOP/scope.py
+++ pythonOOP/scope.py
@@ -2,13 +2,14 @@ conversionUnit = 129
 currencyName = "Ksh"
 
 
-def usd_to_ksh(amount):
+def usd_to_ksh (amount):
     print (f"\n ---------------CONVERTING {amount} USD into KSH---------------")
     print (f"|              {amount} USD is equal to {currencyName}.{amount * conversionUnit}.              |")
     print ("-----------------------------------------------------------")
 
 
-usd_to_ksh(20)
-usd_to_ksh(100)
-usd_to_ksh(4025)
-usd_to_ksh(12650)
+def scope_check (amount):
+    print (f"Global Variable: {conversionUnit}")
+    print (f"Local Variable: {amount}")
+
+scope_check(20)
