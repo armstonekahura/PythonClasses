a11b5201e002c3b0e0dc7bfa2e886e9f1618ebe0
armstonekahura <armstonekahura@gmail.com>
2025-07-18 17:28:36 +0300

    nestedIfElse added

--- pythonOOP/conditionalsIfElse.py
+++ pythonOOP/conditionalsIfElse.py
@@ -5,10 +5,19 @@ currencyName = "Ksh"
 def usd_to_ksh (amount):
     if amount > 0:
         return (f"{amount} USD is equal to {currencyName}.{amount * conversionUnit}.")
+    elif amount == 0:
+        return "⚠️  You entered a 0, kindly input a valid amount"
+    
 
+def validate_and_execute():
+    if userInput.isdigit():
+        userInputNumber = int (userInput)
+        converted_usd_to_ksh = usd_to_ksh(userInputNumber)
+        print(converted_usd_to_ksh)
+    else:
+        print("⚠️   You didn't insert a number as the amount")
+ 
 
 userInput = input ("Kindly insert amount in Dollars to be converted. \nAmount    :   ")
-userInputNumber = int (userInput)
-converted_usd_to_ksh = usd_to_ksh(userInputNumber)
+validate_and_execute()
 
-print(converted_usd_to_ksh)
---
+++ pythonOOP/nestedIfElse.py
@@ -0,0 +1,23 @@
+conversionUnit = 129
+currencyName = "Ksh"
+
+
+def usd_to_ksh (amount):
+    return (f"{amount} USD is equal to {currencyName}.{amount * conversionUnit}.")
+    
+
+def validate_and_execute():
+    if userInput.isdigit():
+        userInputNumber = int(userInput)
+        if userInputNumber > 0:
+             converted_usd_to_ksh = usd_to_ksh(userInputNumber)
+             print(converted_usd_to_ksh)
+        elif userInputNumber == 0:
+            print("⚠️  You entered a 0, kindly input a valid amount")
+    else:
+        print("⚠️   You didn't insert a number as the amount")
+ 
+
+userInput = input ("Kindly insert amount in Dollars to be converted. \nAmount    :   ")
+validate_and_execute()
+
