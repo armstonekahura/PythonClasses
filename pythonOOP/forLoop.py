2943430e23dc499c17a8e1a3035346637d3a738c
armstonekahura <armstonekahura@gmail.com>
2025-07-24 13:58:04 +0300

    forLoop.py added

---
+++ pythonOOP/forLoop.py
@@ -0,0 +1,28 @@
+conversionUnit = 129
+currencyName = "Ksh"
+
+
+def usd_to_ksh (amount):
+    return (f"{amount} USD is equal to {currencyName}.{amount * conversionUnit}.")
+    
+
+def validate_and_execute():
+    try:
+        userInputNumber = int(userInput)
+        if userInputNumber > 0:
+             converted_usd_to_ksh = usd_to_ksh(userInputNumber)
+             print(converted_usd_to_ksh)
+        elif userInputNumber == 0:
+            print("⚠️  You entered a 0, kindly input a valid amount")
+        else:
+            print("⚠️  You entered a negative amount, kindly input a valid amount")
+    except ValueError:
+        print("\n⚠️   You didn't insert a number as the amount")
+ 
+
+userInput = ""
+while userInput != "exit":
+    userInput = input ("Kindly insert amount in Dollars to be converted. \nAmount    :   ")
+    for amount in userInput:
+        validate_and_execute()
+
