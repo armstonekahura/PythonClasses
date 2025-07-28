4aca2f0d5b801c5e3594562d5b23afcb0a64be56
armstonekahura <armstonekahura@gmail.com>
2025-07-28 14:26:15 +0300

    functions1.py added

---
+++ pythonOOP/functions1.py
@@ -0,0 +1,15 @@
+# def multiply ():
+#     a = 3
+#     b = 5
+
+#     return a* b
+
+# print(multiply())
+
+
+# using parameters
+def areaOfRectangle(length, width):
+    area = length * width
+    print (area)
+
+areaOfRectangle(8, 3)
