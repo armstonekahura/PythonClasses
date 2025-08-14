9a07cde7a788276a404bea9544d9c1f70af61287
armstonekahura <armstonekahura@gmail.com>
2025-08-14 12:48:08 +0300

    simpleCalc.py

--- pyQt6GUI/lesson3/simpleCalc.py
+++ pyQt6GUI/lesson3/simpleCalc.py
@@ -1,4 +1,5 @@
-from PyQt6.QtWidgets import QApplication, QWidget
+from PyQt6.QtWidgets import QApplication, QWidget, QLabel, QLineEdit, QPushButton
+from PyQt6.QtGui import QFont
 
 import sys
 
@@ -7,9 +8,43 @@ class SimpleCalc(QWidget):
     def __init__(self):
         super().__init__()
 
+        self.initUI()
+
     def initUI(self):
         self.setWindowTitle("ArMarX simpleCalc")
-        self.setGeometry(0, 0, 350, 300)
+        self.setGeometry(0, 0, 400, 300)
+
+        num1Label = QLabel("Insert a number", self)
+        num1Label.move(30, 30)
+
+        self.num1LineEdit = QLineEdit(self)
+        self.num1LineEdit.move(200, 28)
+
+        num2Label = QLabel("Insert another number", self)
+        num2Label.move(30, 80)
+
+        self.num2LineEdit = QLineEdit(self)
+        self.num2LineEdit.move(200, 78)
+
+        answerLabel = QLabel("Answer", self)
+        answerLabel.move(70, 180)
+
+        self.answerLabel = QLabel("0.0", self)
+        self.answerLabel.setFont(QFont("Ubuntu Sans Mono", 60))
+        self.answerLabel.move(180, 130)
+
+        addBtn = QPushButton("Add", self)
+        addBtn.clicked.connect(self.sumCalculation)
+        addBtn.move(200, 260)
+
+    def sumCalculation(self):
+        num1 = float(self.num1LineEdit.text())
+        num2 = float(self.num2LineEdit.text())
+
+        self.answerLabel.setText(str(num1 + num2))
+        self.answerLabel.adjustSize()
+
+
 
 app = QApplication(sys.argv)
 
